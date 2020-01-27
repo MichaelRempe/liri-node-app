43411d6dbcbedcf9474e16bb8e986f4924bc8c3f
MichaelRempe <michaeljrempe@gmail.com>
2020-01-27 09:51:57 -0500

    Weekend updates

--- apiCMDs.js
+++ apiCMDs.js
@@ -1,5 +1,9 @@
 const axios = require('axios'); //import axios module
-
+require("dotenv").config(); // hide my spotify keys
+const Spotify = require('node-spotify-api');//import spotify api
+const keys = require("./keys"); //import keys
+const spotify = new Spotify(keys.spotify);//implement my keys
+ 
 // API CALL TO 'bandsintown' endpoint
 var accessConcerts = (query)=>{
     let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
@@ -11,18 +15,31 @@ var accessConcerts = (query)=>{
 }
 // API CALL TO Spotify endpoint
 var accessSpotify = (query)=>{
-    let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
-    axios.get(url).then((response)=>{
-        console.log(response);
-    }, (error)=>{
-        console.log(error);
-    });
+   spotify.search({ type: 'track', query: query }, function(error, response) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(response.tracks.items[0]); 
+    console.log("Album Title: "+response.tracks.items[0].album.name);
+    console.log("Song: "+response.tracks.items[0].name);
+    console.log("Artist/Band: "+response.tracks.items[0].artists[0].name);
+    console.log("preview track here: "+response.tracks.items[0].preview_url);
+});
 }
 // API CALL TO OMDB endpoint
 var accessMoives = (query)=>{
-    let url = `https://rest.bandsintown.com/artists/${query}/events?app_id=codingbootcamp`;
+    let url = `http://www.omdbapi.com/?apikey=32e4766d&s=${query}`;
     axios.get(url).then((response)=>{
-        console.log(response);
+        console.log(response.data);
+        // console.log(response.data.Search);
+        // console.log("Title: "+response.data.Search[0].Title);
+        // console.log("Release Year: "+response.data.Search[0].Year);
+        // console.log("Rating: "+ +"; courtesy of Roten Tomatoes");
+        // console.log("Production Counrty: "+);
+        // console.log("Plot: "+);
+        // const actors = [];
+        // for(let i=0; i< response.data.whatever; i++){ actors.push(response.data.wahtever[i])}
+        // console.log("Actors: "+actors);
     }, (error)=>{
         console.log(error);
     });
--- interface.js
+++ interface.js
@@ -1,43 +1,64 @@
-// require("dotenv").config(); // hide my spotify keys
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
-
-// const axios = require('axios');
 const apiCMDs = require('./apiCMDs');
-
 //Capture request and desired input from CLI
 // Request correct api
 // Return data, write to new file
 
-if(process.argv.length>2){
-    try{
+//If CLI contains paramaters past base node call (node projectName.js)
+if (process.argv.length > 2) {
+    try {
         let cmd = process.argv[2].toLowerCase();
-        let query = ""
-        for(let i=3; i<process.argv.length; i++){
-            query+= process.argv[i].toLowerCase()+" ";
+        let query = "";
+        for (let i = 3; i < process.argv.length; i++) {
+            query += process.argv[i].toLowerCase() + " ";
         }
-        query=query.trim();
+        query = query.trim();
 
-        switch (cmd) {
-            case 'concert':
-                apiCMDs.accessConcerts(query);
-                break;
-            case 'spotify':
-                console.log(query);
-                break;
-            case 'movie':
-                console.log(query);
-                break;
-            case 'random':
-                console.log(query);
-                break;
-            default:
-                console.log("Sorry somthing went wrong...");
+        const runCMD = (cmd) => {
+            console.log(cmd, query);
+            switch (cmd) {
+                case 'concert':
+                    if (query === "") {
+                        query = "Motely Crue";
+                    }
+                    apiCMDs.accessConcerts(query);
+
+                    break;
+                case 'spotify':
+                    if (query = "") {
+                        query = "The Sign, Ace of Base";
+                    }
+                    apiCMDs.accessSpotify(query);
+                    break;
+                case 'movie':
+                    if (query = "") {
+                        query = "Mr.Nobody";
+                    }
+                    apiCMDs.accessMoives(query);
+                    break;
+                case 'random':
+                    console.log("Generating random search...");
+                    //This was just for fun, I wanted to have a visual indicator to display that an api was being called. This isnt actually setup in connection to the promise response, so it runs reguardless but it was still fun to figure out.
+                    let timer = 3;
+                    const countDown = setInterval(() => {
+                        console.log("*");
+                        timer--;
+                        if (timer <= 0) {
+                            clearInterval(countDown);
+                            const cmdList = ["moive", "spotify", "concert"]; // command list
+                            let r = Math.floor(Math.random() * cmdList.length); // random num
+                            runCMD(cmdList[r]); //recall runCMD with random command
+                        }
+                    }, 1000);
+                    break;
+                default:
+                    console.log("Sorry somthing went wrong...");
+            }
         }
+        runCMD(cmd);
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         console.log("Invalid entry, required input: 'command-query'");
     }
-}else{console.log("Invalid Entry")}
+} else { console.log("Invalid Entry, must provide command: [movie-query, concert-query, spotify-query]") }
 
